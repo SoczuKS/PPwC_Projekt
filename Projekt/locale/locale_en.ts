75da28216476fe0277044d0164f426aae0624570
SoczuKS <soczynskik@gmail.com>
2023-04-18 10:31:24 +0200

    Add basic translation files

---
+++ Projekt/locale/locale_en.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>AppClass</name>
+    <message>
+        <location filename="../qt/App.ui" line="16"/>
+        <source>App</source>
+        <translation>Vehicle rental office</translation>
+    </message>
+</context>
+</TS>
---
+++ Projekt/locale/locale_pl.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>AppClass</name>
+    <message>
+        <location filename="../qt/App.ui" line="16"/>
+        <source>App</source>
+        <translation>Wypożyczalnia pojazdów</translation>
+    </message>
+</context>
+</TS>
