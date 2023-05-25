065b183bfe47b6e4a58ee7eba1e6306538aa3c10
SoczuKS <soczynskik@gmail.com>
2023-05-25 17:18:35 +0200

    Add loading translations

--- Projekt/src/main.cpp
+++ Projekt/src/main.cpp
@@ -1,9 +1,19 @@
-#include "App.hpp"
 #include <QtWidgets/QApplication>
+#include <QLocale>
+#include <QTranslator>
+
+#include "App.hpp"
 
 int main(int argc, char* argv[])
 {
 	QApplication a(argc, argv);
+
+	QTranslator translator;
+	if (translator.load("locale_" + QLocale::system().name()))
+	{
+		a.installTranslator(&translator);
+	}
+
 	App app;
 	app.show();
 	return a.exec();
