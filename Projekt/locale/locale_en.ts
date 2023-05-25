5293996ac0fbd5eaa46409f25b3dcd8813da76ac
SoczuKS <soczynskik@gmail.com>
2023-05-25 17:19:22 +0200

    Welcome screen UI

--- Projekt/locale/locale_en.ts
+++ Projekt/locale/locale_en.ts
@@ -4,9 +4,29 @@
 <context>
     <name>AppClass</name>
     <message>
-        <location filename="../qt/App.ui" line="16"/>
+        <location filename="../qt/App.ui" line="14"/>
         <source>App</source>
         <translation>Vehicle rental office</translation>
+    </message>
+    <message>
+        <location filename="../qt/App.ui" line="33"/>
+        <source>username</source>
+        <translation>username</translation>
+    </message>
+    <message>
+        <location filename="../qt/App.ui" line="52"/>
+        <source>password</source>
+        <translation>password</translation>
+    </message>
+    <message>
+        <location filename="../qt/App.ui" line="65"/>
+        <source>sign_in</source>
+        <translation>sign in</translation>
+    </message>
+    <message>
+        <location filename="../qt/App.ui" line="81"/>
+        <source>sign_up</source>
+        <translation>sign up</translation>
     </message>
 </context>
 </TS>
--- Projekt/locale/locale_pl.ts
+++ Projekt/locale/locale_pl.ts
@@ -4,9 +4,29 @@
 <context>
     <name>AppClass</name>
     <message>
-        <location filename="../qt/App.ui" line="16"/>
+        <location filename="../qt/App.ui" line="14"/>
         <source>App</source>
         <translation>Wypożyczalnia pojazdów</translation>
+    </message>
+    <message>
+        <location filename="../qt/App.ui" line="33"/>
+        <source>username</source>
+        <translation>nazwa użytkownika</translation>
+    </message>
+    <message>
+        <location filename="../qt/App.ui" line="52"/>
+        <source>password</source>
+        <translation>hasło</translation>
+    </message>
+    <message>
+        <location filename="../qt/App.ui" line="65"/>
+        <source>sign_in</source>
+        <translation>zaloguj</translation>
+    </message>
+    <message>
+        <location filename="../qt/App.ui" line="81"/>
+        <source>sign_up</source>
+        <translation>zarejestruj</translation>
     </message>
 </context>
 </TS>
